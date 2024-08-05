214929df1554c96251ea16ea7741f20161eacbec
Halal Kharbouch <binyaminbgod@gmail.com>
2024-08-05 11:26:37 +0100

    complet google OAuth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,48 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { FcGoogle } from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { db } from '../firebase.config';
+import { useNavigate } from 'react-router';
 
 function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+
+      console.log(user);
+
+      if (docSnap.exists()) {
+        navigate('/');
+        toast.success('Logged in with Google');
+      } else {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+        navigate('/');
+        toast.success('Logged in with Google');
+      }
+    } catch (error) {
+      toast.error('Could not authorize with Google');
+      console.error(error);
+    }
+  }
+
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
-      <FcGoogle className='text-2xl bg-white rounded-full mr-2' />
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue With Google
     </button>
   );
